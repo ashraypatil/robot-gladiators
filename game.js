a717b08a19ac01a2ba0220119619b634ad612403
Ashray <ashraypatil0415@gmail.com>
2022-12-15 03:52:59 -0800

    MVP complete

--- game.js
+++ game.js
@@ -73,6 +73,13 @@ var fight = function(enemyName) {
 };
 
 //END OF FIGHT FUNCTION AND WHILE LOOP 
+// function to start a new game
+var startGame = function() {
+
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
 for (var i = 0; i < enemyNames.length; i++) {
 if (playerHealth > 0) {
@@ -95,4 +102,34 @@ if (playerHealth > 0) {
     break;
   }
 }
+ // play again
+  endGame();
+
+};
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+
+}
+
+
+// start the game when the page loads
+startGame();
 
